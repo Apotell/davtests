7bc0ad4cf9feea7b553ba21126e281b08aa8513c
HS <hs@apotell.com>
2026-05-25 23:32:32 -0700

    Make build location a cmake configurable parameter and fix credits header

--- hlc/PrettyPrint/PrettyPrint/test_PrettyPrint.cpp
+++ hlc/PrettyPrint/PrettyPrint/test_PrettyPrint.cpp
@@ -1,5 +1,5 @@
 /*
- Copyright 2021 Alain Dargelas
+ Copyright 2020 Apotell
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
